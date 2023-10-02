3e30ed780b72bb418ccbf64d0ed11048eebef47e
aLouis Home <alouis@stagiaire-humanbooster.com>
2023-10-02 23:50:02 +0200

    CRUD debugged, hash& TODO

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,7 +4,14 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -22,23 +29,52 @@ class UserType extends AbstractType
                     'employé•e' => 'ROLE_USER'
                 ]])
             ->add('password')
+            // ->add('plainPassword', RepeatedType::class, [
+            //     // No mapping the password instead of being set onto the object directly,
+            //     // this is read and hashed in the controller
+            //     'mapped' => false,
+            //     'type' => PasswordType::class,
+            //     'invalid_message' => 'The password fields must match.',
+            //     'options' => ['attr' => ['class' => 'password-field']],
+            //     'required' => true,
+            //     'first_options'  => ['label' => 'Password'],
+            //     'second_options' => ['label' => 'Repeat Password'],
+            //     // Il faut définir les Constraints ici comme le champ n'est pas mappé
+            //     'constraints' => [
+            //         new NotBlank([
+            //             'message' => 'Please enter a password',
+            //         ]),
+            //         new Length([
+            //             'min' => 6,
+            //             'minMessage' => 'Your password should be at least {{ limit }} characters',
+            //             // max length allowed by Symfony for security reasons
+            //             'max' => 4096,
+            //         ]),
+            //     ],
+            // ])
             ->add('firstname')
             ->add('lastname')
             ->add('personnal_picture')
             //Problème dans le form "The selected choice is invalid" apparait par défaut au dessus du selecteur de choix.
             ->add('departement', ChoiceType::class, [
                 'choices' => [
-                    'RH' => 'DEPARTMENT_RH',
-                    'Direction ' => 'DEPARTMENT_DIRECTION',
-                    'Informatique' => 'DEPARTMENT_INFORMATIQUE',
-                    'Comptabilité' => 'DEPARTMENT_COMPTABILITE'
+                    'Sélection du département' => 'contract_type',
+                    'RH' => 'RH',
+                    'DIRECTION ' => 'DIRECTION',
+                    'INFORMATIQUE' => 'INFORMATIQUE',
+                    'COMPTABILITE' => 'COMPTABILITE',
                 ]])
             //Problème dans le form "The selected choice is invalid" apparait par défaut au dessus du selecteur de choix.
+            //->add('foo', 'choice', array(
+            // 'choices' => array($obj1, $obj2),
+            // 'choice_labels' => 'name',
+            // 'choice_values' => 'id',
             ->add('contract_type', ChoiceType::class, [
                 'choices' => [
-                    'CDI' => 'CONTRACT_TYPE_CDI',
-                    'CDD' => 'CONTRACT_TYPE_CDD',
-                    'Intérim' => 'CONTRACT_TYPE_INTERIM'
+                    'Type de Contrat' => 'contract_type',
+                    'CDI' => 'CDI',
+                    'CDD' => 'CDD',
+                    'INTERIM' => 'INTERIM',
                 ]])
             ->add('contract_end')
         ;
